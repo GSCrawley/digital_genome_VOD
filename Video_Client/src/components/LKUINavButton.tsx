f386dd050187a68d79dd9f89c1ff6de3f71cedb9
GSCrawley <gidstube@gmail.com>
2024-04-09 18:38:00 -0700

    added open source video player to Video Client

---
+++ Video_Client/src/components/LKUINavButton.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useLocation, Link } from 'react-router-dom';
+
+interface LKUINavButtonProps {
+    path: string;
+    regComponent: React.ComponentType;
+    filledComponent: React.ComponentType
+}
+function LKUINavButton({ path, regComponent, filledComponent }: LKUINavButtonProps) {
+    const loc = useLocation()
+    const isDesignatedPage = loc.pathname === path
+    const IconPath = isDesignatedPage ? filledComponent : regComponent
+    return (
+        <Link to={path} className="lkui-path-no-dec">
+            <div className='lkui-nav-button'>
+                <IconPath />
+            </div>
+        </Link>
+    )
+}
+export default LKUINavButton
