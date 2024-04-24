b8de497575bf4b309ac89ab0b284da3ff9563731
GSCrawley <gidstube@gmail.com>
2024-04-24 15:41:30 -0700

    installed front end dependencies

--- Video_Client/video_genome/src/pages/Home.tsx
+++ Video_Client/video_genome/src/pages/Home.tsx
@@ -2,21 +2,21 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import AWS from 'aws-sdk';
 
-
-require('dotenv').config();
+import dotenv from 'dotenv'
+dotenv.config();
 
 
 AWS.config.update({
-  accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-//   secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
-  region: process.env.REACT_APP_AWS_REGION
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
 });
 
 const s3 = new AWS.S3();
 
 const getVideoList = async () => {
     const params = {
-      Bucket: process.env.REACT_APP_S3_BUCKET_NAME,
+      Bucket: process.env.S3_BUCKET_NAME,
     };
   
     try {
