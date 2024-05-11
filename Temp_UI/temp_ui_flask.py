126290454ecf48fe74215bf91369d489b659835b
GSCrawley <gidstube@gmail.com>
2024-05-11 10:29:32 -0400

    created code sharing directory for the sharing of code

--- Temp_UI/temp_ui_flask.py
+++ Temp_UI/temp_ui_flask.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 import requests 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    response = requests.post('http://127.0.0.1:8082/video_url')
-    print(response.json())
-    public_url = response.json()
-    return render_template('index.html', public_url=public_url)
+    # Fetch list of videos from another service or endpoint
+    video_list_response = requests.get('http://127.0.0.1:8082/list_videos')
+    video_list = video_list_response.json().get('urls', [])
+    return render_template('index.html', video_list=video_list)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8081)
