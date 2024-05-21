bf847a3e5029e839e2ce3e2894ef80a9bb40a1c6
GSCrawley <gidstube@gmail.com>
2024-05-21 15:34:16 -0700

    deleted videos from repo history

--- Video_Server_Platform/main.py
+++ Video_Server_Platform/main.py
@@ -29,45 +29,43 @@ def generate_s3_public_url(bucket_name, object_name):
     # The public_url contains the public URL
     return public_url
 
-# def generate_presigned_urls(bucket_name):
-#     # Create an S3 client
-#     s3 = boto3.client('s3')
-    
-#     # Try to fetch the list of objects in the bucket
-#     try:
-#         response = s3.list_objects_v2(Bucket=bucket_name)
-#     except NoCredentialsError:
-#        return "AWS credentials not available", 500
+def generate_presigned_urls(bucket_name):
+# Create an S3 client
+s3 = boto3.client('s3')
 
-#     urls = []
-#     if 'Contents' in response:
-#         # Generate a presigned URL for each video file
-#         for obj in response['Contents']:
-#             video_url = s3.generate_presigned_url('get_object',
-#                                                   Params={'Bucket': 'gidvidbucket', 'Key': obj['Key']},
-#                                                   ExpiresIn=3600)  # URL expires in 1 hour
-#             urls.append(video_url)
-#     else: 
-#         return "No videos found in bucket", 404
+# Try to fetch the list of objects in the bucket
+try:
+response = s3.list_objects_v2(Bucket=bucket_name)
+except NoCredentialsError:
+eturn "AWS credentials not available", 500
 
-#     return urls
+urls = []
+if 'Contents' in response:
+# Generate a presigned URL for each video file
+for obj in response['Contents']:
+video_url = s3.generate_presigned_url('get_object',
+rams={'Bucket': 'gidvidbucket', 'Key': obj['Key']},
+piresIn=3600)  # URL expires in 1 hour
+urls.append(video_url)
+else: 
+return "No videos found in bucket", 404
 
-@app.route('/')
-def index():
-    public_url = generate_s3_public_url('gidvidbucket', 'the_divine_proportion.mp4')
-    # send to temp_ui
-    return public_url
+return urls
 
+# @app.route('/')
+# def index():
+#     public_url = generate_s3_public_url('gidvidbucket', 'the_divine_proportion.mp4')
+#     # send to temp_ui
+#     return public_url
 
-
-# @app.route('/list_videos', methods=['GET'])
-# def list_videos():
-#     bucket_name = 'gidvidbucket'  # Example bucket name
-#     urls = generate_presigned_urls(bucket_name)
-#     if isinstance(urls, str):
-#         # If the function returned a string, it's an error message
-#         return jsonify({'error': urls}), 500
-#     return jsonify({'urls': urls})
+@app.route('/list_videos', methods=['GET'])
+def list_videos():
+bucket_name = 'gidvidbucket'  # Example bucket name
+urls = generate_presigned_urls(bucket_name)
+if isinstance(urls, str):
+# If the function returned a string, it's an error message
+return jsonify({'error': urls}), 500
+return jsonify({'urls': urls})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
